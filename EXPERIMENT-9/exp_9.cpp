9ea6cf19a0638efd1dd0bc142336ebb4cbdad81b
Harshit Kumawat <goldie310805@gmail.com>
2026-04-23 22:17:24 +0530

    Create exp_9.cpp

---
+++ EXPERIMENT-9/exp_9.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    int minDifference(vector<int>& arr) {
+        int n = arr.size();
+        
+        int totalSum = 0;
+        for (int x : arr) totalSum += x;
+        
+        vector<bool> dp(totalSum + 1, false);
+        dp[0] = true; 
+        
+        for (int num : arr) {
+            
+            for (int j = totalSum; j >= num; j--) {
+                dp[j] = dp[j] || dp[j - num];
+            }
+        }
+        int mini = INT_MAX;
+        for (int s1 = 0; s1 <= totalSum / 2; s1++) {
+            if (dp[s1]) {
+                int diff = totalSum - 2 * s1;
+                mini = min(mini, diff);
+            }
+        }
+        
+        return mini;
+    }
+};
